f87b6c1f445522f9c8a61fe475c8df5e8fa56475
Raju-Parchaa <raju.chakaragalla@parchaa.com>
2023-02-24 18:31:02 +0530

    Version with a basic backend

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import { Box, Grid, Stack } from "@mui/material";
-import { useState } from "react";
-import "./index.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [url, setUrl] = useState("https://parchaa.com/");
+  const [url, setUrl] = useState("");
+  const baseUrl = "http://localhost:8080/";
+
+  useEffect(() => {
+    const start = async () => {
+      const link = await axios.get(`${baseUrl}` + "home");
+      setUrl(link.data.url);
+    };
+    start();
+  }, []);
+
+  const handleHome = async () => {
+    const link = await axios.get(`${baseUrl}` + "home");
+    setUrl(link.data.url);
+  };
+  const handleApp = async () => {
+    const link = await axios.get(`${baseUrl}` + "app");
+    setUrl(link.data.url);
+  };
+  const handleAbout = async () => {
+    const link = await axios.get(`${baseUrl}` + "about");
+    setUrl(link.data.url);
+  };
+  const handleContact = async () => {
+    const link = await axios.get(`${baseUrl}` + "contact");
+    setUrl(link.data.url);
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-2">
           <h1>HMIS</h1>
           <div>
-            <button onClick={() => setUrl("https://parchaa.com/")}>Home</button>
+            <button onClick={handleHome}>Home</button>
           </div>
           <div>
-            <button onClick={() => setUrl("https://app.parchaa.com/")}>
-              App
-            </button>
+            <button onClick={handleApp}>App</button>
           </div>
           <div>
-            <button onClick={() => setUrl("https://www.parchaa.com/about")}>
-              About us
-            </button>
+            <button onClick={handleAbout}>About us</button>
           </div>
-          <button onClick={() => setUrl("https://www.parchaa.com/contact")}>
-            Contact Us
-          </button>
+          <button onClick={handleContact}>Contact Us</button>
         </div>
         <div className="col-10">
           <iframe
